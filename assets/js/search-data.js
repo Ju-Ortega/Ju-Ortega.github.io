51998ce30ade4dc1f2d7cff5e2b29635336746fa
Ju-Ortega <92345710+Ju-Ortega@users.noreply.github.com>
2025-12-08 20:24:05 +0000

    Deploying to gh-pages from @ Ju-Ortega/Ju-Ortega.github.io@62554b02a00d4c3790d1abfdf840cb53263d0ddf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-sobre-mí",
-    title: "Sobre mí",
+    id: "nav-about-me",
+    title: "About me",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -440,15 +440,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
